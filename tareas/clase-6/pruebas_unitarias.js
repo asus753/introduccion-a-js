08f611a62a8e7a2b3bb930930474a89b1efe1525
black753 <mauroo_00@hotmail.com>
2019-12-09 02:37:25 -0300

    Correcion error de pruebas_unitarias

--- tareas/clase-6/pruebas_unitarias.js
+++ tareas/clase-6/pruebas_unitarias.js
@@ -1,7 +1,7 @@
 function prueba_validar_integrantes(){
 	console.assert(validar_integrantes(5)==="", "validar_integrantes esta fallando con una cantidad correcta")
 	console.assert(validar_integrantes("Asd")==="Solo se puede ingresar numeros", "validar_integrantes esta tomando como correcto una cantidad incorrecta")
-	console.assert(validar_integrantes(100)==="Demasiados familiares", "validar_integrantes esta tomando demasiados familiares")
+	console.assert(validar_integrantes(100)==="No se admiten numero demasiados grandes", "validar_integrantes esta tomando demasiados familiares")
 	//console.assert(validar_integrantes())
 }
 
